7b66adf012cc3e05751ded55edea12a8212dae54
Furkan5597 <furkann.ismailoglu@gmail.com>
2023-05-17 00:40:38 +0300

    LoginScreen

--- app/src/main/java/com/example/ezystore/MainActivity.java
+++ app/src/main/java/com/example/ezystore/MainActivity.java
@@ -3,14 +3,31 @@ package com.example.ezystore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String Email ;
+    String Password ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView EmailTextView = findViewById(R.id.Emailtext);
+        TextView PasswordTextview = findViewById(R.id.Passwordtext);
+        Button Login = findViewById(R.id.LoginButton);
+
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Email = EmailTextView.getText().toString();
+                Password = PasswordTextview.getText().toString();
+            }
+        });
     }
-    //Deneme
-    //Git deneme
 }
