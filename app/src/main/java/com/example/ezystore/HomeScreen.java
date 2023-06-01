349346c15e9a975c4f8ed5ef8741d9448fc9c0a4
Burakka1 <bkburak45@gmail.com>
2023-06-01 18:31:48 +0300

    düzenlemeler yapıldı

--- app/src/main/java/com/example/ezystore/HomeScreen.java
+++ app/src/main/java/com/example/ezystore/HomeScreen.java
@@ -8,11 +8,6 @@ import android.widget.Button;
 import android.widget.GridView;
 import androidx.appcompat.widget.SearchView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
