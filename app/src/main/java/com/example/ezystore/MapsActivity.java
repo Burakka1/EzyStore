ec5d49df73ce664cd06be1ddec1d0271623d53cd
Burakka1 <bkburak45@gmail.com>
2023-06-05 23:29:42 +0300

    Map Activity tamamlandı

--- app/src/main/java/com/example/ezystore/MapsActivity.java
+++ app/src/main/java/com/example/ezystore/MapsActivity.java
@@ -1,8 +1,20 @@
 package com.example.ezystore;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,32 +22,113 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.ezystore.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import android.Manifest;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private Button sendLocationButton;
     private GoogleMap mMap;
-private ActivityMapsBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-     binding = ActivityMapsBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
-
+        sendLocationButton = findViewById(R.id.sendLocationButton);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        sendLocationButton.setText("Send Location");
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        getCurrentLocation();
+    }
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private String getAddressFromLocation(Location location) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        String address = "";
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses != null && addresses.size() > 0) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                    sb.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+
+                address = sb.toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return address;
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                LatLng currentLatLng = new LatLng(location.getLatitude(),
+                                        location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(currentLatLng)
+                                        .title("Current Location"));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f));
+                                sendLocationButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent = new Intent(MapsActivity.this, Profile.class);
+                                        intent.putExtra("address", getAddressFromLocation(location));
+                                        intent.putExtra("edit_mode", true);
+                                        startActivity(intent);
+                                    }
+                                });
+                            }
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
+
--- app/src/main/java/com/example/ezystore/Profile.java
+++ app/src/main/java/com/example/ezystore/Profile.java
@@ -33,18 +33,19 @@ import java.util.Map;
 public class Profile extends AppCompatActivity {
 
     private TextView textFullName, textPhoneNumber;
-    private EditText editFullName, editPhoneNumber, editAdress;
-    private Button btnEdit, btnSave;
+    private EditText editFullName, editPhoneNumber, editAddress;
+    private Button btnEdit, btnSave,btnSave2;
     private ImageButton cart2,home2,profile;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+    private boolean isEditMode = false;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
         cart2 = findViewById(R.id.cart2);
         home2 = findViewById(R.id.home2);
         profile = findViewById(R.id.Profile);
@@ -52,9 +53,14 @@ public class Profile extends AppCompatActivity {
         textPhoneNumber = findViewById(R.id.textPhoneNumber);
         editFullName = findViewById(R.id.editFullName);
         editPhoneNumber = findViewById(R.id.editPhoneNumber);
-        editAdress = findViewById(R.id.editAddress);
+        editAddress = findViewById(R.id.editAddress);
         btnEdit = findViewById(R.id.btnEdit);
         btnSave = findViewById(R.id.btnSave);
+        btnSave2=findViewById(R.id.btnSave2);
+
+        String address = getIntent().getStringExtra("address");
+        isEditMode = getIntent().getBooleanExtra("edit_mode", false);
+        editAddress.setText(address);
 
         // Firebase Firestore ve Auth örnekleri al
         db = FirebaseFirestore.getInstance();
@@ -62,6 +68,7 @@ public class Profile extends AppCompatActivity {
         currentUser = mAuth.getCurrentUser();
 
         invalidateOptionsMenu();
+
         // Firestore'dan kullanıcının bilgilerini al ve EditText'lere yerleştir
         if (currentUser != null) {
             String userEmail = currentUser.getEmail();
@@ -77,11 +84,15 @@ public class Profile extends AppCompatActivity {
                                 String fullName = documentSnapshot.getString("FullName");
                                 String phoneNumber = documentSnapshot.getString("PhoneNumber");
                                 String email = documentSnapshot.getString("Email");
-                                String adress = documentSnapshot.getString("Adress");
+                                String Adress = documentSnapshot.getString("Adress");
 
                                 editFullName.setText(fullName);
                                 editPhoneNumber.setText(phoneNumber);
-                                editAdress.setText(adress);
+                                editAddress.setText(Adress);
+
+                                if (isEditMode) {
+                                    enableEditMode();
+                                }
                             }
                         }
                     })
@@ -92,24 +103,25 @@ public class Profile extends AppCompatActivity {
                         }
                     });
         }
+
         home2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, HomeScreen.class);
                 startActivity(intent);
                 finish();
-
             }
         });
+
         cart2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, Cart.class);
                 startActivity(intent);
                 finish();
-
             }
         });
+
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -123,7 +135,6 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Kaydet düğmesine tıklanınca EditText'teki bilgiler güncellenir ve tekrar devre dışı bırakılır
-
                 if (validatePhoneNumber()) {
                     updateUserInfo();
                     disableEditMode();
@@ -131,55 +142,35 @@ public class Profile extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.menu_item1) {
-            Intent intent = new Intent(Profile.this, past_orders.class);
-            startActivity(intent);
-            finish();
-            return true;
-        } else if (id == R.id.menu_item2) {
-            Intent intent = new Intent(Profile.this, PasswordReset.class);
-            startActivity(intent);
-            finish();
-            return true;
-        } else if (id == R.id.menu_item3) {
-            Intent intent = new Intent(Profile.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        btnSave2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void enableEditMode() {
         editFullName.setEnabled(true);
         editPhoneNumber.setEnabled(true);
-        editAdress.setEnabled(true);
+        editAddress.setEnabled(true);
+        btnSave.setVisibility(View.VISIBLE);
+        btnSave2.setVisibility(View.VISIBLE);
     }
 
     private void disableEditMode() {
         editFullName.setEnabled(false);
         editPhoneNumber.setEnabled(false);
-        editAdress.setEnabled(false);
+        editAddress.setEnabled(false);
     }
 
     private void updateUserInfo() {
         // EditText'teki bilgileri al ve Firebase Firestore'e güncelleme yap
         String updatedFullName = editFullName.getText().toString();
         String updatedPhoneNumber = editPhoneNumber.getText().toString();
-        String updatedAdress = editAdress.getText().toString();
+        String updatedAddress = editAddress.getText().toString();
 
         if (currentUser != null) {
             String userEmail = currentUser.getEmail();
@@ -198,7 +189,7 @@ public class Profile extends AppCompatActivity {
                                 Map<String, Object> updatedInfo = new HashMap<>();
                                 updatedInfo.put("FullName", updatedFullName);
                                 updatedInfo.put("PhoneNumber", updatedPhoneNumber);
-                                updatedInfo.put("Adress", updatedAdress);
+                                updatedInfo.put("Adress", updatedAddress);
 
                                 db.collection("informations")
                                         .document(documentId)
@@ -236,4 +227,3 @@ public class Profile extends AppCompatActivity {
         return true;
     }
 }
-
