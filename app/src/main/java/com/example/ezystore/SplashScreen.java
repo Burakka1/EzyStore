1e4e18e8e4fb717395a739cea6337835e0306841
Burakka1 <bkburak45@gmail.com>
2023-06-07 12:58:25 +0300

    Splash Screen oluşturuldu Media ögesi eklendi

---
+++ app/src/main/java/com/example/ezystore/SplashScreen.java
@@ -0,0 +1,75 @@
+package com.example.ezystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mediaPlayer = MediaPlayer.create(this, R.raw.soundsplash); // "SoundSplash" yerine ses dosyanızın ismini kullanın
+        mediaPlayer.start();
+
+        ImageView logoImageView = findViewById(R.id.Ezystore_logo);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation fadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+
+        fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logoImageView.startAnimation(fadeOutAnimation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        fadeOutAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logoImageView.setVisibility(View.GONE);
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        logoImageView.startAnimation(fadeInAnimation);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
